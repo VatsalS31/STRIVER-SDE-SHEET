8b7217eba4155b86fd4c80ebc79f95ce7ce53b9d
VatsalS31 <120210435+VatsalS31@users.noreply.github.com>
2024-12-23 13:16:53 +0530

    Create countinversion.cpp

---
+++ Arrays/countinversion.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h> 
+int cnt=0;
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt+=(mid-left+1);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+long long getInversions(long long *arr, int n){
+    mergeSort(a,0,n-1);
+    return cnt;
+}
+
+// merge sort krke har iteration pe hum check krege ki wo agr uska(left subpart) 1st element bada hai right subpart ke 1st element se toh
+// sare elements bade honge that is cnt me wo sare add krdege 
+// TC O(NLOGN) SC O(N) TEMP ARRAY
