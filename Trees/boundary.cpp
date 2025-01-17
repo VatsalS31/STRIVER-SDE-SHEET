59ed9230d87c87a85128bbbc7a804926897feb73
VatsalS31 <120210435+VatsalS31@users.noreply.github.com>
2025-01-17 17:00:32 +0530

    Create boundary.cpp

---
+++ Trees/boundary.cpp
@@ -0,0 +1,97 @@
+class Solution {
+  public:
+  bool isLeaf(Node* root) {
+        return !root->left && !root->right;
+    }
+    void addLeftBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->left;
+        while (curr) {
+            // If the current node is not a leaf,
+            // add its value to the result
+            if (!isLeaf(curr)) {
+                res.push_back(curr->data);
+            }
+            // Move to the left child if it exists,
+            // otherwise move to the right child
+            if (curr->left) {
+                curr = curr->left;
+            } else {
+                curr = curr->right;
+            }
+        }
+    }
+
+    // Function to add the
+    // right boundary of the tree
+    void addRightBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->right;
+        vector<int> temp;
+        while (curr) {
+            // If the current node is not a leaf,
+            // add its value to a temporary vector
+            if (!isLeaf(curr)) {
+                temp.push_back(curr->data);
+            }
+            // Move to the right child if it exists,
+            // otherwise move to the left child
+            if (curr->right) {
+                curr = curr->right;
+            } else {
+                curr = curr->left;
+            }
+        }
+        // Reverse and add the values from
+        // the temporary vector to the result
+        for (int i = temp.size() - 1; i >= 0; --i) {
+            res.push_back(temp[i]);
+        }
+    }
+
+    // Function to add the
+    // leaves of the tree
+    void addLeaves(Node* root, vector<int>& res) {
+        // If the current node is a
+        // leaf, add its value to the result
+        if (isLeaf(root)) {
+            res.push_back(root->data);
+            return;
+        }
+        // Recursively add leaves of
+        // the left and right subtrees
+        if (root->left) {
+            addLeaves(root->left, res);
+        }
+        if (root->right) {
+            addLeaves(root->right, res);
+        }
+    }
+
+
+    vector<int> boundaryTraversal(Node *root) {
+        vector<int> res;
+        if (!root) {
+            return res;
+        }
+        // If the root is not a leaf,
+        // add its value to the result
+        if (!isLeaf(root)) {
+            res.push_back(root->data);
+        }
+
+        // Add the left boundary, leaves,
+        // and right boundary in order
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+
+        return res;
+    }
+};
+
+/*
+ISME HUM LEFT ME TRAVERSE KIYA WITHOUT LEAF ANFD PHIR WE TOOK ALL LEAF NODES AND THEN WE TOOK ALL RIGHT NODES WITHOUT LEAF AND THAT TOO IN
+REVERSE ORDERFOR THAT WE USE NORMAL TRAVERSAL AND FOR LEAF NODES WE USE INORDER TRAVERSAL...
+
+TC O(H)+O(H)+O(N)
+SC O(N)
+*/
